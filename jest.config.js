c643c85ff009becc046dfa86a634b80151b97e35
Mark Stacey <markjstacey@gmail.com>
2021-05-04 14:27:27 -0230

    Collect coverage metrics for all files (#44)
    We now collect coverage metrics for all files, rather than just those
    executed by tests.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
   coverageReporters: ['text', 'html'],
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
   testEnvironment: 'node',
