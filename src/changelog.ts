62b50fbe3d1ef1344b1fd7e415fcc7a8d17b8592
Mark Stacey <markjstacey@gmail.com>
2021-05-24 18:10:16 -0230

    Fix changelog where first release isn't first (#76)
    `auto-changelog` will now correctly update and validate changelogs
    where the first release chronologically is different than the first
    release numerically.
    
    A test has been added to ensure such a changelog is now validated
    correctly. This test fails on `main`.
    
    Fixes #57

--- src/changelog.ts
+++ src/changelog.ts
@@ -109,13 +109,16 @@ function stringifyLinkReferenceDefinitions(
   repoUrl: string,
   releases: ReleaseMetadata[],
 ) {
-  const releasesOrderedByVersion = releases
+  // A list of release versions in descending SemVer order
+  const descendingSemverVersions = releases
     .map(({ version }) => version)
     .sort((a: Version, b: Version) => {
       return semver.gt(a, b) ? -1 : 1;
     });
-  const orderedReleases = releases.map(({ version }) => version);
-  const hasReleases = orderedReleases.length > 0;
+  const latestSemverVersion = descendingSemverVersions[0];
+  // A list of release versions in chronological order
+  const chronologicalVersions = releases.map(({ version }) => version);
+  const hasReleases = chronologicalVersions.length > 0;
 
   // The "Unreleased" section represents all changes made since the *highest*
   // release, not the most recent release. This is to accomodate patch releases
@@ -129,7 +132,7 @@ function stringifyLinkReferenceDefinitions(
   // the link definition.
   const unreleasedLinkReferenceDefinition = `[${unreleased}]: ${
     hasReleases
-      ? getCompareUrl(repoUrl, `v${releasesOrderedByVersion[0]}`, 'HEAD')
+      ? getCompareUrl(repoUrl, `v${latestSemverVersion}`, 'HEAD')
       : withTrailingSlash(repoUrl)
   }`;
 
@@ -139,20 +142,21 @@ function stringifyLinkReferenceDefinitions(
   // patch releases on older releases can be accomodated.
   const releaseLinkReferenceDefinitions = releases
     .map(({ version }) => {
-      if (version === orderedReleases[orderedReleases.length - 1]) {
-        return `[${version}]: ${getTagUrl(repoUrl, `v${version}`)}`;
+      let diffUrl;
+      if (version === chronologicalVersions[chronologicalVersions.length - 1]) {
+        diffUrl = getTagUrl(repoUrl, `v${version}`);
+      } else {
+        const versionIndex = chronologicalVersions.indexOf(version);
+        const previousVersion = chronologicalVersions
+          .slice(versionIndex)
+          .find((releaseVersion: Version) => {
+            return semver.gt(version, releaseVersion);
+          });
+        diffUrl = previousVersion
+          ? getCompareUrl(repoUrl, `v${previousVersion}`, `v${version}`)
+          : getTagUrl(repoUrl, `v${version}`);
       }
-      const versionIndex = orderedReleases.indexOf(version);
-      const previousVersion = orderedReleases
-        .slice(versionIndex)
-        .find((releaseVersion: Version) => {
-          return semver.gt(version, releaseVersion);
-        });
-      return `[${version}]: ${getCompareUrl(
-        repoUrl,
-        `v${previousVersion}`,
-        `v${version}`,
-      )}`;
+      return `[${version}]: ${diffUrl}`;
     })
     .join('\n');
   return `${unreleasedLinkReferenceDefinition}\n${releaseLinkReferenceDefinitions}${
