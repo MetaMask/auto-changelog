546e495c2f481fcc4e27cfb101f1bb7c3a982c17
Mark Stacey <markjstacey@gmail.com>
2021-05-24 11:40:38 -0230

    Add `init` command
    This command generates an empty changelog. It doesn't list any changes
    or releases.
    
    Closes #14

---
+++ src/init.ts
@@ -0,0 +1,13 @@
+import Changelog from './changelog';
+
+/**
+ * Creates a new empty changelog.
+ *
+ * @param options
+ * @param options.repoUrl - The GitHub repository URL for the current project.
+ * @returns The initial changelog text.
+ */
+export function createEmptyChangelog({ repoUrl }: { repoUrl: string }) {
+  const changelog = new Changelog({ repoUrl });
+  return changelog.toString();
+}
