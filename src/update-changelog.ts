MetaMask/auto-changelog

commit ef503a40bf990e16753119588597f7dbe8283610

Move PR to end of change entry (#66)
The PR associated with a change has been moved to the end of the change
entry, rather than being used as the prefix. This matches the "Keep A
Changelog" style better, and generally makes the changelog easier to
read. It also makes it easier to handle the case where one change was
related to multiple PRs.

Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>

src/update-changelog.ts
@@ -203,8 +203,8 @@ export async function updateChangelog({
 
   const newChangeEntries = newCommits.map(({ prNumber, description }) => {
     if (prNumber) {
-      const prefix = `[#${prNumber}](${repoUrl}/pull/${prNumber})`;
-      return `${prefix}: ${description}`;
+      const suffix = `([#${prNumber}](${repoUrl}/pull/${prNumber}))`;
+      return `${description} ${suffix}`;
     }
     return description;
   });

commit 499801681d160abd60031c43a6635cf3898b02c2

Fix PR number detection in existing change entries
The `update` command will filter out any commits that are the result of
PRs that have been referenced already in the changelog.

Unfortunately this PR number detection only looked at the beginning of
the entry. This was because prior to #66, that's where it would be by
default (though this was always a poor decision, because it was always
possible to update the change entry and reference the PR anywhere).
This is especially broken since #66.

We now look for things that look like PR numbers anywhere in the change
description.

src/update-changelog.ts
@@ -88,7 +88,7 @@ function getAllLoggedPrNumbers(changelog: Changelog) {
   const prNumbersWithChangelogEntries = [];
   for (const description of changeDescriptions) {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const matchResults = description!.match(/^\[#(\d+)\]/u);
+    const matchResults = description!.match(/\[#(\d+)\]/u);
     if (matchResults === null) {
       continue;
     }