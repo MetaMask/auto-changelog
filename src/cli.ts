35d48c1d212f2f25ecf0e5170b2121f23a8e0ba7
Mark Stacey <markjstacey@gmail.com>
2021-06-30 13:02:28 -0230

    Update ESLint config for TypeScript and Jest to v7
    The TypeScript update required adding an ESLint ignore comment for the
    URL import. A comment has been added explaining why we're doing that.

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,10 @@
 
 import { promises as fs, constants as fsConstants } from 'fs';
 import path from 'path';
+// Intentionally shadowing 'URL' global, which is equivalent
+// Can't use global directly because of missing type, see:
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34960
+// eslint-disable-next-line @typescript-eslint/no-shadow
 import { URL } from 'url';
 import semver from 'semver';
 import yargs from 'yargs/yargs';
