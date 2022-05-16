e48ffd05f188d9b3c99a9fee9d712e48bdbde9da
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2022-05-16 18:28:56 +0200

    Read repository from package.json if `npm_package_repository_url` is not set (#111)
    * Read repository from package.json if npm_package_repository_url is not set
    
    * Remove console.log

---
+++ src/repo.ts
@@ -0,0 +1,39 @@
+/* eslint-disable node/no-process-env, node/no-sync */
+
+import path from 'path';
+import fs from 'fs';
+
+interface PackageJson {
+  repository:
+    | string
+    | {
+        url: string;
+      };
+}
+
+export function getRepositoryUrl(): string | null {
+  // Set automatically by NPM or Yarn 1.x
+  const npmPackageRepositoryUrl = process.env.npm_package_repository_url;
+  if (npmPackageRepositoryUrl) {
+    return npmPackageRepositoryUrl.replace(/\.git$/u, '');
+  }
+
+  // Set automatically by Yarn 3.x
+  const projectCwd = process.env.PROJECT_CWD;
+  if (projectCwd) {
+    const packageJson = path.resolve(projectCwd, 'package.json');
+    const packageJsonContent = JSON.parse(
+      fs.readFileSync(packageJson, 'utf8'),
+    ) as PackageJson;
+
+    if (typeof packageJsonContent.repository === 'string') {
+      return packageJsonContent.repository.replace(/\.git$/u, '');
+    }
+
+    if (typeof packageJsonContent.repository?.url === 'string') {
+      return packageJsonContent.repository.url.replace(/\.git$/u, '');
+    }
+  }
+
+  return null;
+}
