MetaMask/auto-changelog

commit 4804d3e95e54a51b1cb246df9836aab27bda8aa7

Remove `as` assertions, or fix to mitigate the risk of silent failure (#158)
* Replace `as T[]` assertions with `?? []`

- `as` assertions intended to exclude an empty/nullable type can be replaced by a nullish coalescing operator converting nullable values into an acceptable empty value that doesn't pollute the variable's type signature.
- TODO: document and expound in https://github.com/MetaMask/contributor-docs/issues/47

* Replace `as` assertions with redundant type guards

Sometimes TypeScript's type narrowing and inference doesn't fully work quite like it should. Even in these cases, using `as` assertions should be avoided.

It may seem like we know for certain what the type should be and there's no risk of it breaking, but there's always the possibility that some combination of changes elsewhere might affect the typing, in which case `as` will suppress the alerts TypeScript would otherwise provide us with. This may lead to code breaking silently.

In this case, adding type guards and null checks -- even if they're redundant -- is preferable to the above scenario.

- TODO: document and expound in https://github.com/MetaMask/contributor-docs/issues/47

* Make `as` assertion rely on inference instead of hard-coded type casting

If anything changes in the typing of `ChangeCategory` or `unreleasedChanges` that make them inconsistent, this code will fail silently. Relying on type inference instead of explicit type casting will make these line a little less brittle.

- TODO: document and expound in https://github.com/MetaMask/contributor-docs/issues/47

* Fix type narrowing for `currentVersion` by consolidating conditional branches

- more consistent logic for `isReleaseCandidate` null check
- wasted processing overhead due to errors throwing later than strictly necessary

* Install `@metamask/utils` as a devDep

* Replace `Object.keys` with `getKnownPropertyNames` method

src/update-changelog.ts
@@ -197,11 +197,6 @@ export async function updateChangelog({
   tagPrefixes = ['v'],
   formatter = undefined,
 }: UpdateChangelogOptions): Promise<string | undefined> {
-  if (isReleaseCandidate && !currentVersion) {
-    throw new Error(
-      `A version must be specified if 'isReleaseCandidate' is set.`,
-    );
-  }
   const changelog = parseChangelog({
     changelogContent,
     repoUrl,
@@ -214,15 +209,6 @@ export async function updateChangelog({
   const mostRecentTag = await getMostRecentTag({
     tagPrefixes,
   });
-
-  if (
-    isReleaseCandidate &&
-    mostRecentTag === `${tagPrefixes[0]}${currentVersion || ''}`
-  ) {
-    throw new Error(
-      `Current version already has tag, which is unexpected for a release candidate.`,
-    );
-  }
 
   const commitRange =
     mostRecentTag === null ? 'HEAD' : `${mostRecentTag}..HEAD`;
@@ -247,38 +233,47 @@ export async function updateChangelog({
     return undefined;
   }
 
-  // Ensure release header exists, if necessary
-  if (
-    isReleaseCandidate &&
-    !changelog
-      .getReleases()
-      .find((release) => release.version === currentVersion)
-  ) {
-    // Typecast: currentVersion will be defined here due to type guard at the
-    // top of this function.
-    changelog.addRelease({ version: currentVersion as Version });
-  }
-
-  if (isReleaseCandidate && hasUnreleasedChanges) {
-    // Typecast: currentVersion will be defined here due to type guard at the
-    // top of this function.
-    changelog.migrateUnreleasedChangesToRelease(currentVersion as Version);
-  }
-
-  const newChangeEntries = newCommits.map(({ prNumber, description }) => {
-    if (prNumber) {
-      const suffix = `([#${prNumber}](${repoUrl}/pull/${prNumber}))`;
-      return `${description} ${suffix}`;
+  if (isReleaseCandidate) {
+    if (!currentVersion) {
+      throw new Error(
+        `A version must be specified if 'isReleaseCandidate' is set.`,
+      );
     }
-    return description;
-  });
 
-  for (const description of newChangeEntries.reverse()) {
-    changelog.addChange({
-      version: isReleaseCandidate ? currentVersion : undefined,
-      category: ChangeCategory.Uncategorized,
-      description,
+    if (mostRecentTag === `${tagPrefixes[0]}${currentVersion ?? ''}`) {
+      throw new Error(
+        `Current version already has a tag ('${mostRecentTag}'), which is unexpected for a release candidate.`,
+      );
+    }
+
+    // Ensure release header exists, if necessary
+    if (
+      !changelog
+        .getReleases()
+        .find((release) => release.version === currentVersion)
+    ) {
+      changelog.addRelease({ version: currentVersion });
+    }
+
+    if (hasUnreleasedChanges) {
+      changelog.migrateUnreleasedChangesToRelease(currentVersion);
+    }
+
+    const newChangeEntries = newCommits.map(({ prNumber, description }) => {
+      if (prNumber) {
+        const suffix = `([#${prNumber}](${repoUrl}/pull/${prNumber}))`;
+        return `${description} ${suffix}`;
+      }
+      return description;
     });
+
+    for (const description of newChangeEntries.reverse()) {
+      changelog.addChange({
+        version: isReleaseCandidate ? currentVersion : undefined,
+        category: ChangeCategory.Uncategorized,
+        description,
+      });
+    }
   }
 
   return changelog.toString();

src/changelog.ts
@@ -1,3 +1,4 @@
+import { getKnownPropertyNames } from '@metamask/utils';
 import semver from 'semver';
 
 import {
@@ -89,7 +90,7 @@ function stringifyRelease(
   const categorizedChanges = orderedChangeCategories
     .filter((category) => categories[category])
     .map((category) => {
-      const changes = categories[category] as string[];
+      const changes = categories[category] ?? [];
       return stringifyCategory(category, changes);
     })
     .join('\n\n');
@@ -383,11 +384,11 @@ export default class Changelog {
 
     const unreleasedChanges = this.#changes[unreleased];
 
-    for (const category of Object.keys(unreleasedChanges) as ChangeCategory[]) {
+    for (const category of getKnownPropertyNames(unreleasedChanges)) {
       if (releaseChanges[category]) {
         releaseChanges[category] = [
-          ...(unreleasedChanges[category] as string[]),
-          ...(releaseChanges[category] as string[]),
+          ...(unreleasedChanges[category] ?? []),
+          ...(releaseChanges[category] ?? []),
         ];
       } else {
         releaseChanges[category] = unreleasedChanges[category];

commit 3008846cede8b58eed1ad3afbc4c4e8b66026e74

[update-changelog] Fix bug introduced in #158
- `updateChangelog` returns results even if `isReleaseCandidate` is false

src/update-changelog.ts
@@ -205,6 +205,11 @@ export async function updateChangelog({
   formatter = undefined,
   packageRename,
 }: UpdateChangelogOptions): Promise<string | undefined> {
+  if (isReleaseCandidate && !currentVersion) {
+    throw new Error(
+      `A version must be specified if 'isReleaseCandidate' is set.`,
+    );
+  }
   const changelog = parseChangelog({
     changelogContent,
     repoUrl,
@@ -242,14 +247,8 @@ export async function updateChangelog({
     return undefined;
   }
 
-  if (isReleaseCandidate) {
-    if (!currentVersion) {
-      throw new Error(
-        `A version must be specified if 'isReleaseCandidate' is set.`,
-      );
-    }
-
-    if (mostRecentTag === `${tagPrefixes[0]}${currentVersion ?? ''}`) {
+  if (isReleaseCandidate && currentVersion) {
+    if (mostRecentTag === `${tagPrefixes[0]}${currentVersion}`) {
       throw new Error(
         `Current version already has a tag ('${mostRecentTag}'), which is unexpected for a release candidate.`,
       );
@@ -267,22 +266,22 @@ export async function updateChangelog({
     if (hasUnreleasedChanges) {
       changelog.migrateUnreleasedChangesToRelease(currentVersion);
     }
+  }
 
-    const newChangeEntries = newCommits.map(({ prNumber, description }) => {
-      if (prNumber) {
-        const suffix = `([#${prNumber}](${repoUrl}/pull/${prNumber}))`;
-        return `${description} ${suffix}`;
-      }
-      return description;
-    });
-
-    for (const description of newChangeEntries.reverse()) {
-      changelog.addChange({
-        version: isReleaseCandidate ? currentVersion : undefined,
-        category: ChangeCategory.Uncategorized,
-        description,
-      });
+  const newChangeEntries = newCommits.map(({ prNumber, description }) => {
+    if (prNumber) {
+      const suffix = `([#${prNumber}](${repoUrl}/pull/${prNumber}))`;
+      return `${description} ${suffix}`;
     }
+    return description;
+  });
+
+  for (const description of newChangeEntries.reverse()) {
+    changelog.addChange({
+      version: isReleaseCandidate ? currentVersion : undefined,
+      category: ChangeCategory.Uncategorized,
+      description,
+    });
   }
 
   return changelog.toString();