b7c5c0e60921b3a14a020e492e8bbd0194af0697
Elliot Winkler <elliot.winkler@gmail.com>
2025-03-05 11:00:35 -0700

    Add way to verify each change has associated PRs (#222)
    This commit adds a new option to the `validate` command, `--pr-links`,
    which will cause an error to be thrown if:
    
    - a changelog entry does not have one or more links to pull requests
    after it
    - a changelog entry does have PR links present, but they do not point to
    the project's repo
    - a changelog entry does have PR links present, but they are not
    positioned at the very end of the line
    
    The `ensureValidPrLinksPresent` option has also been added to
    `validateChangelog`.
    
    If this option is provided, then `parseChangelog` is instructed to look
    for and extract pull request numbers from changelog entries. The list of
    numbers will then be checked for in the validation step. It is also used
    to reconstruct pull request links when the changelog is stringified.
    
    Note that because this commit changes what `parseChangelog` returns,
    this is a breaking change.

--- src/changelog.test.ts
+++ src/changelog.test.ts
@@ -1,4 +1,9 @@
+import _outdent from 'outdent';
+
 import Changelog from './changelog';
+import { ChangeCategory } from './constants';
+
+const outdent = _outdent({ trimTrailingNewline: false });
 
 const emptyChangelog = `# Changelog
 All notable changes to this project will be documented in this file.
@@ -27,5 +32,44 @@ describe('Changelog', () => {
     });
 
     expect(await changelog.toString()).toStrictEqual(emptyChangelog);
+  });
+
+  it('should recreate pull request links for change entries based on the repo URL', async () => {
+    const changelog = new Changelog({
+      repoUrl: 'https://github.com/MetaMask/fake-repo',
+    });
+    changelog.addRelease({ version: '1.0.0' });
+    changelog.addChange({
+      version: '1.0.0',
+      category: ChangeCategory.Changed,
+      description: 'This is a cool change\n  - This is a sub-bullet',
+      prNumbers: ['100', '200'],
+    });
+    changelog.addChange({
+      version: '1.0.0',
+      category: ChangeCategory.Changed,
+      description: 'This is a very cool change\nAnd another line',
+      prNumbers: ['300'],
+    });
+
+    expect(await changelog.toString()).toStrictEqual(outdent`
+      # Changelog
+      All notable changes to this project will be documented in this file.
+
+      The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+      and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+      ## [Unreleased]
+
+      ## [1.0.0]
+      ### Changed
+      - This is a very cool change ([#300](https://github.com/MetaMask/fake-repo/pull/300))
+      And another line
+      - This is a cool change ([#100](https://github.com/MetaMask/fake-repo/pull/100), [#200](https://github.com/MetaMask/fake-repo/pull/200))
+        - This is a sub-bullet
+
+      [Unreleased]: https://github.com/MetaMask/fake-repo/compare/v1.0.0...HEAD
+      [1.0.0]: https://github.com/MetaMask/fake-repo/releases/tag/v1.0.0
+    `);
   });
 });
